1b797afb0a1e02708bf5565558942c62484ec8cb
saad gaber <saad.gaber1994@gmail.com>
2024-06-13 01:24:38 +0300

    final commit

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ var webSiteName = document.getElementById("siteName");
 var webSiteUrl = document.getElementById("webURL");
 var btn = document.getElementById("btn");
 var deleteBtn = document.getElementById("deleteBtn");
+var closeBtn = document.getElementById("closeBtn");
+var boxModal = document.querySelector(".box-info");
 wList = [];
 // ?111111111111111111111111111111111
 
@@ -13,11 +15,21 @@ if (localStorage.getItem("web") !== null) {
 }
 
 function addWeb() {
-  if (webSiteName.value == "" || webSiteUrl.value == "") return;
+  if (webSiteName.value == "" || webSiteUrl.value == "") {
+    boxModal.classList.remove("d-none");
+    return;
+  }
 
   console.log(validateURL(webSiteUrl.value));
 
   if (validateURL(webSiteUrl.value) == false) {
+    check();
+    boxModal.classList.remove("d-none");
+    return;
+  }
+  if (validateName(webSiteName.value) == false) {
+    checkName();
+    boxModal.classList.remove("d-none");
     return;
   }
 
@@ -29,6 +41,7 @@ function addWeb() {
   console.log(wList);
   displayUrl();
   clearForm();
+
   localStorage.setItem("web", JSON.stringify(wList));
 }
 btn.addEventListener("click", addWeb);
@@ -41,10 +54,10 @@ function displayUrl() {
     <td> ${[i + 1]}</td>
     <td> ${wList[i].name}</td>
 
-    <td> <a target="_blank"  href="${
+    <td>  <button class=" btn-visit btn "> <a class="text-decoration-none text-white" target="_blank"  href="${
       wList[i].wURL
-    }">Visit<i class="fa-solid fa-eye pe-2"></i> </a> </td>
-   <td> <button id='deleteBtn' onclick="deleteWebSite(${i})" class="btn btn-outline-warning"><i class="fa-solid fa-trash-can"></i> Delete</button> </td>
+    }"><i class="fa-solid fa-eye pe-2"></i>Visit </a> </button> </td>
+   <td> <button id='deleteBtn' onclick="deleteWebSite(${i})" class="btn btn-danger"><i class="fa-solid fa-trash-can"></i> Delete</button> </td>
     </tr>
     `;
   }
@@ -65,16 +78,67 @@ function deleteWebSite(index) {
   localStorage.setItem("web", JSON.stringify(wList));
 }
 // ???/
-function addHttps(samir) {
-  if (!samir) {
-    return "https://www." + samir;
+
+function addHttps(value) {
+  if (value) {
+    return "https://www." + value;
   }
 
-  return samir;
+  return value;
 }
 
 function validateURL(value) {
   const validator = /^[a-zA-Z0-9]+\.[a-zA-Z0-9]+$/;
-
   return validator.test(value);
 }
+
+function validateName(nVlaue) {
+  const validator1 = /^\w{3,}(\s+\w+)*$/;
+  return validator1.test(nVlaue);
+}
+webSiteName.addEventListener("input", checkName);
+webSiteUrl.addEventListener("input", check);
+
+function check() {
+  webSiteUrl;
+  webSiteUrl.classList.remove("is-valid", "is-invalid");
+  if (validateURL(webSiteUrl.value)) {
+    webSiteUrl.classList.add("is-valid");
+    webSiteUrl.nextElementSibling.classList.replace("d-block", "d-none");
+  } else {
+    webSiteUrl.classList.add("is-invalid");
+
+    webSiteUrl.nextElementSibling.classList.replace("d-none", "d-block");
+  }
+}
+function checkName() {
+  webSiteName;
+  webSiteName.classList.remove("is-valid", "is-invalid");
+  if (validateName(webSiteName.value)) {
+    webSiteName.classList.add("is-valid");
+    webSiteName.nextElementSibling.classList.replace("d-block", "d-none");
+  } else {
+    webSiteName.classList.add("is-invalid");
+    webSiteName.nextElementSibling.classList.replace("d-none", "d-block");
+  }
+}
+
+function closeModal() {
+  boxModal.classList.add("d-none");
+}
+
+// 3 ways to close modal => close button -  Esc key - clicking outside modal
+
+closeBtn.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key == "Escape") {
+    closeModal();
+  }
+});
+
+document.addEventListener("click", function (e) {
+  if (e.target.classList.contains("box-info")) {
+    closeModal();
+  }
+});
